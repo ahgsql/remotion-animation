5a2e053a9979f991e326de53884b004419992b4a
Juan Pablo Mejia <juanpmd@hotmail.com>
2021-04-09 16:14:39 -0500

    feat(dev environment): add storybook configuration

---
+++ .storybook/Component.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { CounterComponent } from '../src/index.js';
+
+export default {
+	component: CounterComponent,
+	title: 'Counter'
+};
+
+export const Placeholder = () => <CounterComponent />;
